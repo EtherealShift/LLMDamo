0b5c19b734a1b949df623b1f74718767ee2ebcca
Lxd <lxd020901@gmail.com>
2026-04-20 09:45:10 +0800

    通过Http访问LLM简单对话

---
+++ src/main/java/com/example/llmdamo/HttpClientCaller.java
@@ -0,0 +1,53 @@
+package com.example.llmdamo;
+
+import org.w3c.dom.UserDataHandler;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientCaller {
+
+    private static final String API_KEY = "sk-ea2194eb2be34b18b41b91f37ff9c22e";
+    private static final String API_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String requestBody = """
+                {
+                    "model": "qwen-plus",
+                    "messages": [
+                        {
+                            "role": "system",
+                            "content": "You are a helpful assistant."
+                        },
+                        {
+                            "role": "user",
+                            "content": "你好，介绍下JAVA？"
+                        }
+                    ],
+                    "stream": true
+                }
+                """;
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer" + API_KEY)
+                .header("X-DashScope-SSE", "enable")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+
+        HttpResponse<String> response = client.send(
+                request, HttpResponse.BodyHandlers.ofString()
+        );
+
+
+
+        System.out.println(response.body());
+    }
+}
---
+++ src/main/java/com/example/llmdamo/LlmDamoApplication.java
@@ -0,0 +1,13 @@
+package com.example.llmdamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmDamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmDamoApplication.class, args);
+    }
+
+}
